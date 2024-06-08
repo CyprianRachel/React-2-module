24b35058f14bc4a055e109cd3613f72fa733d0df
CyprianRachel <cyprianrachel@gmail.com>
2024-06-08 16:45:55 +0200

    Hooks

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,9 +3,11 @@ import { useState } from "react";
 function App() {
   const [isSpoilerShown, setIsSpoilerShown] = useState(false);
   const [isWarningShown, setIsWarningShown] = useState(true);
+  const [numberOfLikes, setNumberOfLikes] = useState(50);
 
   function handleShowSpoilerClick() {
     setIsSpoilerShown(true);
+    console.log("handleClick!");
     setIsWarningShown(false);
   }
 
@@ -13,10 +15,23 @@ function App() {
     setIsWarningShown(false);
   }
 
+  function handleLikeButtonClick() {
+    setNumberOfLikes((previousNumberOfLikes) => previousNumberOfLikes + 1);
+  }
+
+  function handleLoveButtonClick() {
+    setNumberOfLikes((previousNumberOfLikes) => previousNumberOfLikes + 3);
+  }
+
+  console.log("<App> - render");
+
   return (
     <>
-      <h1>Gwiezdne wojny 5</h1>
+      <h1>Gwiezdne wojny V</h1>
       <h2>Rok produkcji: 1980</h2>
+      <h2>Liczba polubień: {numberOfLikes}</h2>
+      <button onClick={handleLikeButtonClick}>Lubię to!</button>
+      <button onClick={handleLoveButtonClick}>Kocham to!</button>
       <h2>Fabuła</h2>
       {isWarningShown && (
         <p>
