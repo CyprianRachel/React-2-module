3176d0d0a63c859e7d1cc6e1bd8457c580b66080
CyprianRachel <cyprianrachel@gmail.com>
2024-06-08 16:06:08 +0200

    UseState!

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,16 @@
+import { useState } from "react";
+
 function App() {
-  function handleClick() {
-    alert("Klik!");
+  const [isSpoilerShown, setIsSpoilerShown] = useState(false);
+  const [isWarningShown, setIsWarningShown] = useState(true);
+
+  function handleShowSpoilerClick() {
+    setIsSpoilerShown(true);
+    setIsWarningShown(false);
+  }
+
+  function handleCloseWarningClick() {
+    setIsWarningShown(false);
   }
 
   return (
@@ -8,9 +18,18 @@ function App() {
       <h1>Gwiezdne wojny 5</h1>
       <h2>Rok produkcji: 1980</h2>
       <h2>Fabuła</h2>
+      {isWarningShown && (
+        <p>
+          Uwaga! Opis fabuły zawiera spoilery!
+          <button onClick={handleCloseWarningClick}>X</button>
+        </p>
+      )}
       <p>Dobrzy walczą ze złymi. Trzeba wyłączyć pole siłowe.</p>
-      <button onClick={handleClick}>Pokaż spoiler</button>
-      <p>Vader okazuje się być ojcem Luka.</p>
+      {isSpoilerShown ? (
+        <p>Vader okazuje się być ojcem Luka.</p>
+      ) : (
+        <button onClick={handleShowSpoilerClick}>Pokaż spoiler</button>
+      )}
     </>
   );
 }
